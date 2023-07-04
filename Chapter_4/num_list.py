588693a3645c7c5b4b32cc37aa2986f1c1fca10a
Alfred <alfredgachanja8@gmail.com>
2023-07-04 17:14:16 +0300

    I complete chapter 4 today

--- Chapter_4/num_list.py
+++ Chapter_4/num_list.py
@@ -9,20 +9,24 @@ for value in range(1, 6):
 
 #Using range function to make a list.
 number = list(range(1, 6))
+
 print(number)
 
 #Printing even numbers using the range function.
 even_num = list(range(2, 11, 2))
+
 print("\nEven numbers:")
 print(even_num)
 
 #Printing odd numbers using the range function.
 odd_num = list(range(1, 11, 2))
+
 print("\nOdd numbers:")
 print(odd_num)
 
 #Square numbers using the range function.
 squares = []
+
 for value in range(1, 11):
     squares.append(value**2)
 
@@ -39,21 +43,25 @@ for value in range(1, 21):
     print(value)
 
 num = list(range(1, 1000001))
+
 print("\nThe minumum number in the list is: " + str(min(num)))
 print("The maximum number in the list is:" + str(max(num)))
 print("The sum from one to a million is:" + str(sum(num)) + "\n")
 
 odd_num = list(range(1, 20, 2))
+
 print("Odd numbers:")
 for value in odd_num:
     print(value)
 
 multiple_3 = list(range(3, 30, 3))
+
 print("\nMultiples of three:")
 for three in multiple_3:
     print(three)
 
 cubes = []
+
 for cube in range(1,10):
     cubes.append(cube**3)
 
@@ -61,5 +69,7 @@ print("\nThe third power")
 print(cubes)
 
 print("\nList Compression.")
+
 cubes = [cube**3 for cube in range(1,10)]
+
 print(cubes)
