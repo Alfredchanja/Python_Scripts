cfba5941c70b20ef4536146ab92dabb349102c9a
Alfred <alfredgachanja8@gmail.com>
2024-01-07 17:14:06 +0300

    my first actual project.

---
+++ Data Analysis with Python Programming Projects/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -0,0 +1,17 @@
+"""
+Alfred Gachanja
+07-01-2024
+This is a function that uses Numpy to calculate
+ the mean, variance, standard deviation, max, min,
+ and sum of the rows, columns, and elements in a
+ 3 by 3 matrix.
+"""
+
+import numpy as np
+
+def calculate(digits):
+
+
+
+
+    return calculate
