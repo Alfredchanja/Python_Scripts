e8de739365d5c918d00e28dbfbc67ab0336c650e
Alfred <alfredgachanja8@gmail.com>
2024-01-24 20:47:58 +0300

    coding one line at a time

---
+++ Data Analysis with Python Programming Projects/Medical Data Visualizer/medical_data_visualizer.py
@@ -0,0 +1,18 @@
+"""
+Alfred Gachanja
+24-01-2024
+This program shows a visual representation of some medical data.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data
+df = pd.read_csv('medical_examination.csv')
+
+# Add 'overweight' column.
+BMI = (df['weight']/((df['height'])/100)**2)
+df['overweight'] = (BMI)
+
+print(df.head())
